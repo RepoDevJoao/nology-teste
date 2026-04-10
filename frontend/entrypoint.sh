d1d0b0df8bc7e691e33ecfb503559ddbeb61b0d3
João Vitor <joaovitortargueta@gmail.com>
2026-04-10 08:56:11 -0300

    fix: usar envsubst para injetar BACKEND_URL no frontend

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-# Injeta a variável de ambiente BACKEND_URL no index.html em runtime
-# Isso permite que o frontend saiba a URL do backend sem rebuild
+export BACKEND_URL=${BACKEND_URL:-"http://localhost:8000"}
 
-BACKEND_URL=${BACKEND_URL:-"http://localhost:8000"}
+envsubst '${BACKEND_URL}' < /usr/share/nginx/html/index.html.tmpl \
+    > /usr/share/nginx/html/index.html
 
-# Usa arquivo temporário para evitar problemas com barras na URL
-TMP=$(mktemp)
-sed "s|window.BACKEND_URL || \"http://localhost:8000\"|\"${BACKEND_URL}\"|g" \
-    /usr/share/nginx/html/index.html > "$TMP"
-mv "$TMP" /usr/share/nginx/html/index.html
+chmod 644 /usr/share/nginx/html/index.html
 
 nginx -g "daemon off;"
