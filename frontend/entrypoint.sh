025ee21e23228af5f33f4fb639bcdc238277042c
João Vitor <joaovitortargueta@gmail.com>
2026-04-10 08:22:48 -0300

    fix: corrigir sed no entrypoint do frontend

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -4,7 +4,10 @@
 
 BACKEND_URL=${BACKEND_URL:-"http://localhost:8000"}
 
-sed -i "s|window.BACKEND_URL || \"http://localhost:8000\"|\"${BACKEND_URL}\"|g" \
-    /usr/share/nginx/html/index.html
+# Usa arquivo temporário para evitar problemas com barras na URL
+TMP=$(mktemp)
+sed "s|window.BACKEND_URL || \"http://localhost:8000\"|\"${BACKEND_URL}\"|g" \
+    /usr/share/nginx/html/index.html > "$TMP"
+mv "$TMP" /usr/share/nginx/html/index.html
 
 nginx -g "daemon off;"
