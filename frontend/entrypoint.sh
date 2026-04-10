2953e52632c984395da18ccc534fb9a913e04615
João Vitor <joaovitortargueta@gmail.com>
2026-04-10 07:50:12 -0300

    feat: frontend javascript and backend requirements

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Injeta a variável de ambiente BACKEND_URL no index.html em runtime
+# Isso permite que o frontend saiba a URL do backend sem rebuild
+
+BACKEND_URL=${BACKEND_URL:-"http://localhost:8000"}
+
+sed -i "s|window.BACKEND_URL || \"http://localhost:8000\"|\"${BACKEND_URL}\"|g" \
+    /usr/share/nginx/html/index.html
+
+nginx -g "daemon off;"
